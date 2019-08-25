405f7ee8c8aed7512a5adf1f332da26be4ea966f
Janne Peippo <peippo@lumous.com>
2019-08-25 16:47:53 +0300

    Add osa 3b 3.9 answer

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
 	morgan(function(tokens, req, res) {
