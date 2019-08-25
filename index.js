d5d65330d3cbee79a1a1f46901f35b79fb730a8d
Janne Peippo <peippo@lumous.com>
2019-08-25 16:12:25 +0300

    Add osa 3a 3.7 & 3.8 answers

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 app.use(bodyParser.json());
+app.use(
+	morgan(function(tokens, req, res) {
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, "content-length"),
+			"-",
+			tokens["response-time"](req, res),
+			"ms",
+			tokens.postContent(req, res)
+		].join(" ");
+	})
+);
+
+morgan.token("postContent", function(req, res) {
+	return JSON.stringify(req.body);
+});
 
 const generateId = () => {
 	return Math.floor(Math.random() * 10000000);
